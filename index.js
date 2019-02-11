d1aea040ffc1c33cc881380d4460942d3190b007
siratl <elishubax@gmail.com>
2019-02-11 13:23:36 -0800

    created READ operation of the CRUD operations. .get implimented on api

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // import your node modules
-
+const express = require('express');
+const port = 3333;
 const db = require('./data/db.js');
 
 // add your server code starting here
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('<h1>Server Running...</h1>');
+});
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json({ success: true, posts });
+    })
+    .catch(err => {
+      res.status(err.code).json({ success: false, message: err.message });
+    });
+});
+
+server.listen(port, () => {
+  console.log('\n*** Running on Port 3333 ***\n');
+});
